4d3ccebcc873418e596abb754f454d5435abb7a0
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-07 13:18:00 +0100

    EFCore installed

---
+++ Reichinger.Masterarbeit.Database/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reichinger.Masterarbeit.Database.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Studiengang { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastModified { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
