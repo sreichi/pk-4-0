4d64833a71b0cbb7ea2f655906d69236f4a02044
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-14 12:41:32 +0100

    [NEW] created Test project for Unit Tests

---
+++ src/Reichinger.Masterarbeit.PK-4-0.Test/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Test
+{
+    public class Class1
+    {
+    }
+}
