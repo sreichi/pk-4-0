15b3587749e8ac9d4b80309de8be7c59e3f3233f
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-17 10:49:34 +0100

    [UPDATE] Began to implement DataSeeder

---
+++ src/Reichinger.Masterarbeit.PK-4-0/Database/DataSeeder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Reichinger.Masterarbeit.PK_4_0.Database.Models;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Database
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(this IApplicationBuilder applicationBuilder)
+        {
+            var db = applicationBuilder.ApplicationServices.GetService(typeof (ApplicationDbContext)) as ApplicationDbContext;
+            CreateTestData(db);
+        }
+
+        private static void CreateTestData(ApplicationDbContext dbContext)
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            CreateRoles(dbContext);
+        }
+
+        private static void CreateRoles(ApplicationDbContext dbContext)
+        {
+            dbContext.Role.Add(
+                new Role
+                {
+                    Id = 1,
+                    Name = "Admin"
+                });
+            dbContext.Role.Add(
+                new Role
+                {
+                    Id = 2,
+                    Name = "User"
+                });
+        }
+    }
+}
--- src/Reichinger.Masterarbeit.PK-4-0/Database/Models/AppUser.cs
+++ src/Reichinger.Masterarbeit.PK-4-0/Database/Models/AppUser.cs
@@ -18,41 +18,53 @@ namespace Reichinger.Masterarbeit.PK_4_0.Database.Models
 
         [Column("id")]
         public int Id { get; set; }
+
         [Required]
         [Column("firstname", TypeName = "varchar")]
         [MaxLength(50)]
         public string Firstname { get; set; }
+
         [Required]
         [Column("lastname", TypeName = "varchar")]
         [MaxLength(50)]
         public string Lastname { get; set; }
+
         [Required]
         [Column("email", TypeName = "varchar")]
         [MaxLength(50)]
         public string Email { get; set; }
+
         [Required]
         [Column("password", TypeName = "bpchar")]
         [MaxLength(128)]
         public string Password { get; set; }
+
         [Required]
         [Column("salt_string", TypeName = "varchar")]
         [MaxLength(50)]
         public string SaltString { get; set; }
+
         [Column("mat_nr")]
         public int MatNr { get; set; }
+
         [Column("ldap_id")]
         public int LdapId { get; set; }
+
         [Column("active")]
         public bool? Active { get; set; }
+
         [Column("created")]
         public DateTime Created { get; set; }
 
         [InverseProperty("User")]
         public virtual ICollection<Application> Application { get; set; }
+
         [InverseProperty("User")]
         public virtual ICollection<Asignee> Asignee { get; set; }
+
         [InverseProperty("User")]
         public virtual ICollection<Comment> Comment { get; set; }
+
         [InverseProperty("User")]
         public virtual ICollection<UserHasRole> UserHasRole { get; set; }
     }
