981e4fce3f65992a61a1040dd5bba2af7df301cc
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-21 15:41:26 +0100

    [NEW] Implemented base integration test class and wrote first test testing a route

---
+++ src/Reichinger.Masterarbeit.PK-4-0.Test/Integration/IntegrationTestBase.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Test.Integration
+{
+    public class IntegrationTestBase
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        public IntegrationTestBase()
+        {
+            _server = new TestServer(new WebHostBuilder().UseEnvironment("Development").UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        protected async Task<HttpResponseMessage> GetHttpResult(string path)
+        {
+            return await _client.GetAsync(path);
+        }
+    }
+}
--- src/Reichinger.Masterarbeit.PK-4-0.Test/Integration/UserEndpointTest.cs
+++ src/Reichinger.Masterarbeit.PK-4-0.Test/Integration/UserEndpointTest.cs
@@ -1,7 +1,19 @@
-﻿namespace Reichinger.Masterarbeit.PK_4_0.Test.Integration
-{
-    public class UserEndpointTest
-    {
+﻿using System.Net;
+using FluentAssertions;
+using Xunit;
 
+namespace Reichinger.Masterarbeit.PK_4_0.Test.Integration
+{
+    public class UserEndpointTest: IntegrationTestBase
+    {
+        private const string UrlPath = "/users";
+
+        [Fact]
+        public async void GetAllUsersShouldReturnTwo()
+        {
+            var result = await GetHttpResult(UrlPath);
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
     }
 }
