56a8eefd830cc28b7da35e57342ee4c1db07e8ef
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-23 15:32:34 +0100

    [UPDATE] reverted change because docker is not setting the right environment variable

--- src/Reichinger.Masterarbeit.PK-4-0/Startup.cs
+++ src/Reichinger.Masterarbeit.PK-4-0/Startup.cs
@@ -79,10 +79,8 @@ namespace Reichinger.Masterarbeit.PK_4_0
             app.UseSwagger((httpRequest, swaggerDoc) => { swaggerDoc.Host = httpRequest.Host.Value; });
 
             app.UseSwaggerUi();
-            if (_environment.IsEnvironment("Development") || _environment.IsEnvironment("Travis"))
-            {
-                app.SeedData();
-            }
+
+            app.SeedData();
         }
     }
 }
