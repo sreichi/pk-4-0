b79ef59db673849ea9623193eeb7354c37cb448f
Stephan Reichinger <stephan.reichinger@xitaso.com>
2016-11-23 14:29:41 +0100

    [NEW] conferenceEndpoint integration tests

---
+++ src/Reichinger.Masterarbeit.PK-4-0.Test/Integration/ConferenceEndpointTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Http;
+using Newtonsoft.Json;
+using Reichinger.Masterarbeit.PK_4_0.Database.DataTransferObjects;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Test.Integration
+{
+    public class ConferenceEndpointTest : IntegrationTestBase
+    {
+        private const string UrlPath = "/conferences/";
+        private const int ConferenceId = 1;
+        private const int InvalidConferenceId = 9876543;
+
+
+        public async void GetAllConferencesShouldReturnAListOfConferenceDtos()
+        {
+            var result = await GetHttpResult(UrlPath);
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var conferences = JsonConvert.DeserializeObject<List<ConferenceDto>>(result.Content.ReadAsStringAsync().Result);
+            conferences.ForEach(conference => conference.Should().BeOfType<ConferenceDto>());
+        }
+
+        public async void GetConferenceByIdShouldReturnOneConferenceDto()
+        {
+            var result = await GetHttpResult(UrlPath + ConferenceId);
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var confernce = JsonConvert.DeserializeObject<ConferenceDto>(result.Content.ReadAsStringAsync().Result);
+            confernce.Should().BeOfType<ConferenceDto>();
+        }
+
+        public async void GetConferenceByIdShouldReturnNotFoundForInvalidId()
+        {
+            var result = await GetHttpResult(UrlPath + InvalidConferenceId);
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
