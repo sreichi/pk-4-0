9eec91f4790b67e5f7ca021891543751a5a19187
Stephan Reichinger <stephan.reichinger@xitaso.com>
2017-01-12 11:20:45 +0100

    test

--- src/Reichinger.Masterarbeit.PK-4-0/Startup.cs
+++ src/Reichinger.Masterarbeit.PK-4-0/Startup.cs
@@ -24,6 +24,7 @@ namespace Reichinger.Masterarbeit.PK_4_0
         public Startup(IHostingEnvironment environment)
         {
             _environment = environment;
+            Console.WriteLine(_environment.EnvironmentName);
             var builder = new ConfigurationBuilder()
                 .SetBasePath(environment.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
