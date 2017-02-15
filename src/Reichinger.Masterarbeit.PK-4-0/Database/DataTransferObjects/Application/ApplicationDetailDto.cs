1dd1b30c3df61f09455a0f255eca36895b836906
Stephan Reichinger <stephan.reichinger@xitaso.com>
2017-02-15 13:44:19 +0100

    refactored Conference and Application Dtos

---
+++ src/Reichinger.Masterarbeit.PK-4-0/Database/DataTransferObjects/Application/ApplicationDetailDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Database.DataTransferObjects
+{
+    public class ApplicationDetailDto
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        public string FilledForm { get; set; }
+
+        public int Version { get; set; }
+
+        public bool IsCurrent { get; set; }
+
+        public Guid? PreviousVersion { get; set; }
+
+        public UserDto User { get; set; }
+
+        public ConferenceListDto Conference { get; set; }
+
+        public StatusDto Status { get; set; }
+
+        public FormsDto Form { get; set; }
+
+        public virtual IEnumerable<UserDto> Assignments { get; set; }
+
+        public virtual IEnumerable<CommentDto> Comments { get; set; }
+
+    }
+}
---
+++ src/Reichinger.Masterarbeit.PK-4-0/Database/DataTransferObjects/Conference/ConferenceDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reichinger.Masterarbeit.PK_4_0.Database.DataTransferObjects
+{
+    public class ConferenceDetailDto
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfEvent { get; set; }
+        public DateTime StartOfEvent { get; set; }
+        public DateTime EndOfEvent { get; set; }
+        public string RoomOfEvent { get; set; }
+        public int NumberOfConference { get; set; }
+        public virtual IEnumerable<ApplicationListDto> Applications { get; set; }
+        public virtual IEnumerable<UserDto> Attendants { get; set; }
+    }
+}
